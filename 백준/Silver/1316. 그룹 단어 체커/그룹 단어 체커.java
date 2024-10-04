2565c5b83da16a0161ab9c7fb795fe8d3fc96cec
진현규 <108057548+Hyeonqz@users.noreply.github.com>
2024-10-04 13:50:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+
+		for (int i = 0; i < N ; i++) {
+			String str = br.readLine();
+			if(check(str) == true) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	private static boolean check(String str) throws IOException {
+		boolean[] check = new boolean[26];
+		int prev = 0;
+
+        // char 에 배열 자리에 한번 나왔으면 true 로 바꾼다.
+		for (int i = 0; i <str.length() ; i++) {
+			int now = str.charAt(i);
+
+			if(prev != now) {
+				if(check[now - 'a'] == false) {
+					check[now - 'a'] = true;
+					prev = now;
+				}
+				else {
+					return false;
+				}
+			}
+			else {
+				continue;
+			}
+		}
+		return true;
+	}
+
+}
