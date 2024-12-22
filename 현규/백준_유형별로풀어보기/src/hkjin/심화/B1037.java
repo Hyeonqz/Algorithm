05e9055641cd7e8040b7212aea3127a52a466a3a
hkjin <wlsgusrb78@naver.com>
2024-12-22 21:58:11 +0900

    B1037 약수

--- 현규/백준_유형별로풀어보기/src/hkjin/심화/B1037.java
+++ 현규/백준_유형별로풀어보기/src/hkjin/심화/B1037.java
@@ -6,16 +6,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B1037 {
+	static int N;
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int N = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st =new StringTokenizer(br.readLine());
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 
-			// 입력 -> 배열로 받아서 잘라야 할듯?
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
+		while(T-- > 0) {
+			int N = Integer.parseInt(st.nextToken());
+			max = Math.max(N, max);
+			min = Math.min(N, min);
 		}
+		System.out.println(max * min);
+
 	}
 }
+
+/*
+*  약수 result 는 A 의 배수여야하고, A 는 1과 N 이 아니어야 함
+* */
+
+/*
+* 24 약수 개수
+* 2 3 4 6 8 12
+* 최소공배수 공식처럼? 흠 고민좀 해봐야 할듯?
+* */
