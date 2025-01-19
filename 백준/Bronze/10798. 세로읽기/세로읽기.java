1d7759399ff17131487b65192876325dbaa47561
HyeonKyu Jin <108057548+Hyeonqz@users.noreply.github.com>
2025-01-19 15:42:41 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		char[][] twoChar = new char[5][15];
+		int max = 0;
+
+		for (int i = 0; i < 5; i++) {
+			String s = br.readLine();
+
+			if(max < s.length()) max = s.length();
+
+			for (int j = 0; j < s.length(); j++) {
+				twoChar[i][j] = s.charAt(j);
+			}
+		}
+
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < max; i++) {
+			for (int j = 0; j < 5; j++) {
+				if(twoChar[j][i] == '\0') continue;
+
+				sb.append(twoChar[j][i]);
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
