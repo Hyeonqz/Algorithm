2b897615e26c5b7741d06fd0d3ee1798b151e442
hkjin <wlsgusrb78@naver.com>
2026-03-12 23:56:35 +0900

    Add 해시 문제 '전화번호 목록' solution

---
+++ hkjin/프로그래머스_유형별로풀어보기/src/hkjin/해시/전화번호_목록.java
@@ -0,0 +1,25 @@
+package hkjin.해시;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(solution(phone_book));
+    }
+
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        // 사전식 정렬
+        Arrays.sort(phone_book);
+
+        for (int i =0; i < phone_book.length -1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return answer;
+    }
+
+}
